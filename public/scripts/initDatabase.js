14af04c68516cfd396fc5928ac366b42ac4b5f6f
Unknown <guinet.kevin@gmail.com>
2017-10-29 23:18:12 +0100

    Create new game

---
+++ public/scripts/initDatabase.js
@@ -0,0 +1,52 @@
+var initDatabase = {}
+
+initDatabase["colorPlayer"] = "white";
+
+initDatabase["messages"] = {};
+initDatabase["pieces"] = {};
+
+initDatabase["pieces"]["B_B1"] = "C8"; 
+initDatabase["pieces"]["B_B2"] = "F8"; 
+initDatabase["pieces"]["B_K"] = "E8"; 
+initDatabase["pieces"]["B_N1"] = "B8"; 
+initDatabase["pieces"]["B_N2"] = "G8"; 
+initDatabase["pieces"]["B_P1"] = "A7"; 
+initDatabase["pieces"]["B_P2"] = "B7"; 
+initDatabase["pieces"]["B_P3"] = "C7"; 
+initDatabase["pieces"]["B_P4"] = "D7"; 
+initDatabase["pieces"]["B_P5"] = "E7"; 
+initDatabase["pieces"]["B_P6"] = "F7"; 
+initDatabase["pieces"]["B_P7"] = "G7"; 
+initDatabase["pieces"]["B_P8"] = "H7"; 
+initDatabase["pieces"]["B_Q"] = "D8"; 
+initDatabase["pieces"]["B_R1"] = "A8"; 
+initDatabase["pieces"]["B_R2"] = "H8"; 
+initDatabase["pieces"]["W_B1"] = "C1"; 
+initDatabase["pieces"]["W_B2"] = "F1"; 
+initDatabase["pieces"]["W_K"] = "E1"; 
+initDatabase["pieces"]["W_N1"] = "B1"; 
+initDatabase["pieces"]["W_N2"] = "G1"; 
+initDatabase["pieces"]["W_P1"] = "A2"; 
+initDatabase["pieces"]["W_P2"] = "B2"; 
+initDatabase["pieces"]["W_P3"] = "C2"; 
+initDatabase["pieces"]["W_P4"] = "D2"; 
+initDatabase["pieces"]["W_P5"] = "E2"; 
+initDatabase["pieces"]["W_P6"] = "F2"; 
+initDatabase["pieces"]["W_P7"] = "G2"; 
+initDatabase["pieces"]["W_P8"] = "H2"; 
+initDatabase["pieces"]["W_Q"] = "D1"; 
+initDatabase["pieces"]["W_R1"] = "A1"; 
+initDatabase["pieces"]["W_R2"] = "H1";
+
+initDatabase["piecesMove"] = {};
+initDatabase["piecesMove"]["B_K"] = "false"; 
+initDatabase["piecesMove"]["B_R1"] = "false"; 
+initDatabase["piecesMove"]["B_R2"] = "false"; 
+initDatabase["piecesMove"]["W_K"] = "false"; 
+initDatabase["piecesMove"]["W_R1"] = "false"; 
+initDatabase["piecesMove"]["W_R2"] = "false"; 
+
+initDatabase["status"] = "inGame";  
+initDatabase["time1"] = "inf";  
+initDatabase["time2"] = "inf";  
+initDatabase["time_init"] = "inf"; 
